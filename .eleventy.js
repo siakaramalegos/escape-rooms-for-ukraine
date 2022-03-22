f7bb74ef32420d2f6b0d5679ffe0c601e9c21222
Sia Karamalegos <siakaramalegos@gmail.com>
2022-03-22 14:38:45 -0400

    Set up basic 11ty config

---
+++ .eleventy.js
@@ -0,0 +1,56 @@
+// const fs = require("fs");
+// const filters = require("./src/_11ty/filters");
+// const shortcodes = require("./src/_11ty/shortcodes");
+
+module.exports = function (eleventyConfig) {
+  // Filters
+  // Object.keys(filters).forEach((filterName) => {
+  //   eleventyConfig.addFilter(filterName, filters[filterName]);
+  // });
+
+  // // Shortcodes
+  // Object.keys(shortcodes).forEach((shortcodeName) => {
+  //   eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName]);
+  // });
+
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/javascript");
+
+  // eleventyConfig.setBrowserSyncConfig({
+  //   callbacks: {
+  //     ready: function (err, browserSync) {
+  //       const content_404 = fs.readFileSync("_site/404.html");
+
+  //       browserSync.addMiddleware("*", (req, res) => {
+  //         // Provides the 404 content without redirect.
+  //         res.write(content_404);
+  //         res.end();
+  //       });
+  //     },
+  //   },
+  // });
+
+  return {
+    templateFormats: ["md", "njk", "html"],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "./src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
